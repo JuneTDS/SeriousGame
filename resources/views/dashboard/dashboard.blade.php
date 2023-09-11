4bde05c9d3c34d2581b8a56ff18c3f1d575db0e6
June TDS <tds.mm.dev004@gmail.com>
2023-09-11 09:59:28 +0630

    Upload project

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<h1>{{ $title }}</h1>
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Serious Games</title>
+    <link href="/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+        Menu
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="assets/js/jquery-3.7.1.min.js"></script>
+</body>
+</html>
